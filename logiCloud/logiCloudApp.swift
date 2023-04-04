8fdb65c5442399ee35d5c1fa733df3090a18e76a
Ayush78 <ayusharyal78@gmail.com>
2023-04-04 14:05:25 +0545

    Initial Commit

---
+++ logiCloud/logiCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  logiCloudApp.swift
+//  logiCloud
+//
+//  Created by macbook  on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct logiCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
